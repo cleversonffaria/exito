420d6e247da7184b79ac44236bff2231395ec1da
Cleverson Fernandes <cleversonffaria@gmail.com>
2025-06-09 09:46:06 -0300

    fix: ajustando envs

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -3,8 +3,8 @@ import { secureStorage } from "@/utils/secure-store.utils";
 import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
